83f1274edabb57a124ece1c5daab52aabe42ac68
noadonna <noadonna@users.noreply.github.com>
2020-10-14 11:50:58 +0200

    Imported Project Files

---
+++ script.js
@@ -0,0 +1,164 @@
+//SEARCH ENGINE
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name.toUpperCase();
+  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#temperatureDescription").innerHTML =
+    response.data.weather[0].main;
+    celsiusTemperature = response.data.main.temp;
+  //WEATHER ICON
+  let weatherIcon = document.querySelector(".weatherInfo img");
+  let imgCode = {
+    "01d": "https://ssl.gstatic.com/onebox/weather/256/sunny.png",
+    "01n": "https://ssl.gstatic.com/onebox/weather/256/sunny.png",
+    "02d": "https://ssl.gstatic.com/onebox/weather/256/partly_cloudy.png",
+    "02n": "https://ssl.gstatic.com/onebox/weather/256/partly_cloudy.png",
+    "03d": "https://ssl.gstatic.com/onebox/weather/256/partly_cloudy.png",
+    "03n": "https://ssl.gstatic.com/onebox/weather/256/partly_cloudy.png",
+    "04d": "https://ssl.gstatic.com/onebox/weather/256/cloudy.png",
+    "04n": "https://ssl.gstatic.com/onebox/weather/256/cloudy.png",
+    "09d": "https://ssl.gstatic.com/onebox/weather/256/rain_heavy.png",
+    "09n": "https://ssl.gstatic.com/onebox/weather/256/rain_heavy.png",
+    "10d": "https://ssl.gstatic.com/onebox/weather/256/rain.png",
+    "10n": "https://ssl.gstatic.com/onebox/weather/256/rain.png",
+    "11d": "https://ssl.gstatic.com/onebox/weather/256/thunderstorms.png",
+    "11n": "https://ssl.gstatic.com/onebox/weather/256/thunderstorms.png",
+    "13d": "https://ssl.gstatic.com/onebox/weather/256/snow.png",
+    "13n": "https://ssl.gstatic.com/onebox/weather/256/snow.png",
+    "50d": "https://ssl.gstatic.com/onebox/weather/256/snow.png",
+    "50n": "https://ssl.gstatic.com/onebox/weather/256/snow.png",
+  };
+  weatherIcon.src = imgCode[response.data.weather[0].icon];
+}
+
+function searchCity(city) {
+  let apiKey = "c21c69cfcac20320f0c03c2e080dbc19";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function returnCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#searchCity").value;
+  searchCity(city);
+}
+
+function showLocation(position) {
+  let lon = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let apiKey = "c21c69cfcac20320f0c03c2e080dbc19";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
+
+let city = document.querySelector("#searchForm");
+city.addEventListener("submit", returnCity);
+
+let currentLocation = document.querySelector("#locationButton");
+currentLocation.addEventListener("click", getLocation);
+
+//CELCIUS TO FAHRENHEIT
+/*function celsius(event) {
+  event.preventDefault();
+  let tempCurrent = document.querySelector("#tempLocal");
+  tempCurrent.innerHTML = 24;
+}
+
+function fahrenheit(event) {
+  event.preventDefault();
+  let tempCurrent = document.querySelector("#tempLocal");
+  tempCurrent.innerHTML = 75;
+}
+
+let changeFahr = document.querySelector("#changeFahr");
+changeFahr.addEventListener("click", fahrenheit);
+
+let changeCelc = document.querySelector("#changeCelc");
+changeCelc.addEventListener("click", celsius);
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+*/
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#changeFahr");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#changeCels");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+//DATE FORMULA
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = weekday[now.getDay()];
+
+let curMonth = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = curMonth[now.getMonth()];
+
+h2.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}`;
+
+//FORECAST DATES
+let h4 = document.querySelector("h4");
+
+let forecast = new Date();
+forecast.setDate(forecast.getDate() + 1);
+console.log(forecast);
+
+let dd = forecast.getDate();
+var mm = forecast.getMonth() + 1;
+
+var dateString = dd + "/" + mm;
+h4.innerHTML = `${dateString}`;
